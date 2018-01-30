5490d60007bd49682a7341ee9493a7b860589bcf
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-01-30 14:32:45 +0100

    Update run.sh

--- linux/run.sh
+++ linux/run.sh
@@ -1,9 +1,15 @@
 echo "Starting adb"
-/usr/sbin/adb -a -P 5037 server
+/usr/sbin/adb -a -P 5037 server nodaemon &
+adb_pid=$!
+
+echo "adb is running in the background with pid $adb_pid"
 
 echo "Starting the Thrift API Server"
 cd /app
 java -version
 java nttr.rtk.MainForThriftServer
+
+echo "Stopping adb"
+/usr/sbin/adb kill-server
 
 echo "Exiting"
